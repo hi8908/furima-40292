6cc5cdae4d054794c2a269626b750fa3a8a50156
hi8908 <sibutaku774@gmail.com>
2024-05-09 21:41:35 +0900

    ログイン、ログアウト時のエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  
-  validates :nickname, presence: true 
-  validates :email, presence: true
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
   validates :password, presence: true,length: { minimum: 6 },format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  #全角ひらがな、全角カタカナ、漢字
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龠々ー]+\z/, message: "は全角で入力してください" }
+  validates :first_name, format:{ with: /\A[ぁ-んァ-ン一-龠々ー]+\z/, message: "は全角で入力してください" }
+  # 全角カタカナ
+  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  
   validates :birthday, presence: true
 end
