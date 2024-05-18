5f6eadaeb508a2b7d0318e877f6864c5b1212c47
hi8908 <sibutaku774@gmail.com>
2024-05-18 19:40:34 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   # アクティブハッシュとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :delivery_area
   belongs_to :item_status
