fcfddd3f02c2a7f12f7415db59f9d152f32d54b7
hi8908 <sibutaku774@gmail.com>
2024-05-24 20:42:01 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   # 重複処理をまとめる
-  before_action :set_item, only: [:show, :edit, :update,:destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -41,7 +41,6 @@ class ItemsController < ApplicationController
     @item.destroy if @item.user_id == current_user.id
     redirect_to root_path
   end
-
 
   private
 
