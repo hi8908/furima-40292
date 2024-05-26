f3f4a3c7513c8b5aeb54974c09a7b48d8b245aa6
hi8908 <sibutaku774@gmail.com>
2024-05-26 21:24:46 +0900

    マイグレーションファイルの記述

--- db/migrate/20240526082801_create_payments.rb
+++ db/migrate/20240526082801_create_payments.rb
@@ -1,7 +1,13 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
-
+      t.references :order,         null: false, foreign_key: true
+      t.string     :postcode,      null: false
+      t.integer    :delivery_area_id, null: false
+      t.string     :city,          null: false
+      t.string     :address,         null: false
+      t.string     :building
+      t.string     :phone_number,  null: false
       t.timestamps
     end
   end
