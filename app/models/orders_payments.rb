f82b178c05c9918b677daecb2c29cca25f73ebac
hi8908 <sibutaku774@gmail.com>
2024-05-26 17:00:23 +0900

    Formオブジェクトの作成

---
+++ app/models/orders_payments.rb
@@ -0,0 +1,23 @@
+class OrdersPayments
+  include ActiveModel::Model
+
+  attr_accessor :token, :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :token
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
+  end
+
+  def save
+    # 購入情報を保存
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    # 配送先情報を保存
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
