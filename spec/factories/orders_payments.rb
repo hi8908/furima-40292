234570dad851776206af77befb5286526825562e
hi8908 <sibutaku774@gmail.com>
2024-06-01 21:22:04 +0900

    テストコードの実装

--- spec/factories/orders_payments.rb
+++ spec/factories/orders_payments.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :orders_payments do
-    name { Faker::Name.name }
-    description { Faker::Lorem.sentence }
-    category_id { Faker::Number.between(from: 1, to: 10) }
-    item_status_id { Faker::Number.between(from: 1, to: 6) }
-    shipping_cost_id { Faker::Number.between(from: 1, to: 2) }
-    prefecture_id { Faker::Number.between(from: 1, to: 47) }
-    shipping_date_id { Faker::Number.between(from: 1, to: 3) }
-    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    token { "tok_abcdefghijk00000000000000000" }
+    item_id { Faker::Number.number(digits: 1) }
+    user_id { Faker::Number.number(digits: 1) }
+    postal_code { Faker::Number.number(digits: 3).to_s + "-" + Faker::Number.number(digits: 4).to_s }
+    prefecture_id { Faker::Number.between(from: 2, to: 47) } # 1 is invalid, so start from 2
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    building { Faker::Address.secondary_address }
+    phone_number { Faker::Number.leading_zero_number(digits: 11) }
   end
 end
--- spec/models/orders_payments_spec.rb
+++ spec/models/orders_payments_spec.rb
@@ -1,5 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe OrdersPayments, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @orders_payments = FactoryBot.build(:orders_payments)
+  end
+
+  describe '配送先情報の保存' do
+    context '配送先情報の保存ができるとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@orders_payments).to be_valid
+      end
+      it 'user_idが空でなければ保存できる' do
+        @orders_payments.user_id = 1
+        expect(@orders_payments).to be_valid
+      end
+      it 'item_idが空でなければ保存できる' do
+        @orders_payments.item_id = 1
+        expect(@orders_payments).to be_valid
+      end
+      it '郵便番号が「3桁＋ハイフン＋4桁」の組み合わせであれば保存できる' do
+        @orders_payments.postal_code = '123-4560'
+        expect(@orders_payments).to be_valid
+      end
+      it '都道府県が「---」以外かつ空でなければ保存できる' do
+        @orders_payments.prefecture_id = 2
+        expect(@orders_payments).to be_valid
+      end
+      it '市区町村が空でなければ保存できる' do
+        @orders_payments.city = '横浜市'
+        expect(@orders_payments).to be_valid
+      end
+      it '番地が空でなければ保存できる' do
+        @orders_payments.address = '青山1-1-1'
+        expect(@orders_payments).to be_valid
+      end
+      it '電話番号が11桁以内かつハイフンなしであれば保存できる' do
+        @orders_payments.phone_number = '09012345678'
+        expect(@orders_payments).to be_valid
+      end
+    end
+
+    context '配送先情報の保存ができないとき' do
+      it 'user_idが空だと保存できない' do
+        @orders_payments.user_id = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @orders_payments.item_id = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が空だと保存できないこと' do
+        @orders_payments.postal_code = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Postal code can't be blank", 'Postal code is invalid')
+      end
+      it '郵便番号にハイフンがないと保存できないこと' do
+        @orders_payments.postal_code = '1234567'
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include('Postal code is invalid')
+      end
+      it '都道府県が「---」だと保存できないこと' do
+        @orders_payments.prefecture_id = 1
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '都道府県が空だと保存できないこと' do
+        @orders_payments.prefecture_id = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が空だと保存できないこと' do
+        @orders_payments.city = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @orders_payments.address = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Address can't be blank")
+      end
+      it '電話番号が空だと保存できないこと' do
+        @orders_payments.phone_number = nil
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンがあると保存できないこと' do
+        @orders_payments.phone_number = '123-1234-1234'
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が12桁以上あると保存できないこと' do
+        @orders_payments.phone_number = '123456789012'
+        @orders_payments.valid?
+        expect(@orders_payments.errors.full_messages).to include('Phone number is invalid')
+      end
+    end
+  end
 end
