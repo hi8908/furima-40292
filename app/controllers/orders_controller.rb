fc9435a9b7d5871be612b762f3fd758be657b6cf
hi8908 <sibutaku774@gmail.com>
2024-06-01 19:38:36 +0900

    エラーメッセージのコントローラ修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,7 @@ class OrdersController < ApplicationController
       @orders_payments.save
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
