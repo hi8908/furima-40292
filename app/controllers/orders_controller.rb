825759e2f738f784e94d91c969be6f2540d0ab11
hi8908 <sibutaku774@gmail.com>
2024-06-01 12:34:29 +0900

    ordesコントローラの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,28 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
-  before_action :check_item_sold_out, only: [:index, :create]
 
   def index
-    @order_payment = OrdersPayments.new
+    @order_form = OrdersPayments.new
   end
 
+  def create
+    @order_form = OrdersPayments.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:orders_payments).permit(:token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
