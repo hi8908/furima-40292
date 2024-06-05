8a4b7be00591b208caaca09fe8a842a1d5e7fc46
hi8908 <sibutaku774@gmail.com>
2024-06-05 20:38:08 +0900

    order_paymentsの修正

--- app/models/orders_payments.rb
+++ app/models/orders_payments.rb
@@ -8,7 +8,7 @@ class OrdersPayments
     validates :item_id
     validates :user_id
     validates :postcode, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :delivery_area_id, numericality: { other_than: 1 }
+    validates :delivery_area_id, numericality: { other_than: 0}
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
